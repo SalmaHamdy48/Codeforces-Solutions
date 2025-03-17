10bd23f2c20f3fb00341915cb0b8d4a649af0c93
SalmaHamdy48 <salmahamdy1066@gmail.com>
2025-03-17 22:26:00 +0200

    Added task 2

---
+++ Task2/script2.js
@@ -0,0 +1,12 @@
+
+let myImg = document.getElementsByTagName("img");
+for (let i = 0; i < myImg.length; i++) {
+  if(myImg[i].getAttribute("alt")){
+
+    myImg[i].setAttribute("alt","Old")
+  }
+  else{
+    myImg[i].setAttribute("alt","Elzero New")   
+  }
+}
+
