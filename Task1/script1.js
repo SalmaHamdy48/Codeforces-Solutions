865682ed22a2656e956233713772eec75839614d
SalmaHamdy48 <salmahamdy1066@gmail.com>
2025-03-17 21:45:19 +0200

    Added task 1

---
+++ Task1/script1.js
@@ -0,0 +1,21 @@
+const button = document.getElementById("button");
+
+button.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  const number = document.getElementById("number").value;
+  const text = document.getElementById("textChoice").value;
+  const tagType = document.getElementById("dsChoice").value;
+  const output = document.getElementById("output");
+
+  
+
+
+  
+  for (let i = 0; i < number; i++) {
+    const box = document.createElement(tagType);
+    box.classList.add("box");
+    box.textContent = text;
+    output.appendChild(box);
+  }
+});
