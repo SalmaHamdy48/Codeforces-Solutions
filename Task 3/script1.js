ff7b771d85be10869831a456c180116dd89d5dd2
SalmaHamdy48 <salmahamdy1066@gmail.com>
2025-03-22 22:21:16 +0200

    Added Task 3

---
+++ Task 3/script1.js
@@ -0,0 +1,16 @@
+let myInfo = {
+  username: "Osama",
+  role: "Admin",
+  country: "Egypt",
+};
+
+map = new Map(Object.entries(myInfo));
+console.log(map);
+console.log(map.size);
+console.log(map.has("role"));
+
+// Needed Output
+//Map(3) {'username' => 'Osama', 'role' => 'Admin', 'country' => 'Egypt'}
+//3
+//true
+
