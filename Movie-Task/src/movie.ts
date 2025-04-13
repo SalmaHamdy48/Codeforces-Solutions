0b092e5bd03056260ab52886bc6d26691601ec37
SalmaHamdy48 <salmahamdy1066@gmail.com>
2025-04-14 01:13:14 +0200

    Added Movie Task

---
+++ Movie-Task/src/movie.ts
@@ -0,0 +1,9 @@
+export interface Movie {
+    title: string;
+    overview: string;
+    poster_path: string;
+    backdrop_path: string;
+    vote_average: number;
+    popularity: number;
+    release_date: string;
+}
