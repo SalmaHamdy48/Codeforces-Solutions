ba2e45ee77b48c85e18b616fa479d94ed2a169a5
SalmaHamdy48 <salmahamdy1066@gmail.com>
2025-04-14 00:32:44 +0200

    Added all files of java script

---
+++ Api-Task/dist/movieApp.js
@@ -0,0 +1,74 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+import { MovieService } from './movieService.js';
+import { MovieApi } from './movieApi.js';
+export class MovieApp {
+    constructor() {
+        this.movieService = new MovieService();
+        this.movieApi = new MovieApi();
+        this.allMovies = [];
+        this.movieIndex = 0;
+        this.init();
+    }
+    init() {
+        return __awaiter(this, void 0, void 0, function* () {
+            yield this.loadMovies("spiderman");
+            this.setupEventListeners();
+        });
+    }
+    loadMovies(query) {
+        return __awaiter(this, void 0, void 0, function* () {
+            this.allMovies = yield this.movieService.fetchMovies(query);
+            this.movieIndex = 0;
+            this.render();
+        });
+    }
+    render() {
+        if (this.allMovies.length > 0) {
+            this.movieApi.showMoviesList(this.allMovies, this.movieIndex, (index) => this.onMovieClick(index));
+            this.movieApi.showMovieDetails(this.allMovies[this.movieIndex]);
+            const scroll = document.querySelector('.scroll-list');
+            const posters = scroll.querySelectorAll('.poster');
+            const activePoster = posters[this.movieIndex];
+            if (scroll && activePoster) {
+                const scrollLeft = activePoster.offsetLeft - scroll.offsetWidth / 2 + activePoster.offsetWidth / 2;
+                scroll.scrollTo({ left: scrollLeft, behavior: 'smooth' });
+            }
+        }
+    }
+    onMovieClick(index) {
+        this.movieIndex = index;
+        this.render();
+    }
+    setupEventListeners() {
+        var _a, _b;
+        (_a = document.querySelector(".next")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", () => {
+            if (this.movieIndex < this.allMovies.length - 1) {
+                this.movieIndex++;
+                this.render();
+            }
+        });
+        (_b = document.querySelector(".previous")) === null || _b === void 0 ? void 0 : _b.addEventListener("click", () => {
+            if (this.movieIndex > 0) {
+                this.movieIndex--;
+                this.render();
+            }
+        });
+        const searchInput = document.getElementById("search-input");
+        const searchIcon = document.getElementById("search-icon");
+        searchIcon === null || searchIcon === void 0 ? void 0 : searchIcon.addEventListener("click", () => {
+            searchInput.classList.toggle("active");
+            searchInput.focus();
+        });
+        searchInput === null || searchInput === void 0 ? void 0 : searchInput.addEventListener("input", () => {
+            this.loadMovies(searchInput.value);
+        });
+    }
+}
