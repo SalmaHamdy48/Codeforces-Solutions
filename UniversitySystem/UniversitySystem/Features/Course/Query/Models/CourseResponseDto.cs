bd7a82f3cd7a34406139154bc5014a36820bc91a
Salma <salma.hamdy@appnest.pro>
2025-09-19 20:34:19 +0300

    initial commit

--- UniversitySystem/UniversitySystem/Features/Course/Query/Models/CourseResponseDto.cs
+++
@@ -1,9 +0,0 @@
-namespace UniversitySystem.Features.Course.Query.Models
-{
-    public class CourseResponseDto
-    {
-        public int Id { get; set; }
-        public string Title { get; set; } = string.Empty;
-        public int Credits { get; set; }
-    }
-}
